1374b74904282625f4dbabd48f48654cfb9a9d56
tungli <tun@mail.muni.cz>
2020-01-15 22:59:20 +0100

    complete day5

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,45 +1,82 @@
 defmodule Day5 do
-  
+
 
   @doc """
   iex> Day5.run(0, [1002,4,3,4,33])
   [1002,4,3,4,99]
   """
   def run(at, seq) do
+    IO.inspect(seq)
     {opcode, params} = parse_number(Enum.at(seq, at))
     arg_access = fn () -> params 
                  |> Enum.with_index(1)
                  |> Enum.map(fn {p, i} -> mode(p).(at + i, seq) end) end
     {move, seq} = case opcode do
-      1 -> {4, sum(seq, arg_access.())}
-      2 -> {4, mul(seq, arg_access.())}
+      1 -> sum(seq, arg_access.())
+      2 -> mul(seq, arg_access.())
       99 -> {:stop, seq}
-      3 -> {2, put(IO.gets("input\n"), seq, arg_access.())}
-      4 -> {2, output(seq, arg_access.())}
+      3 -> put(IO.gets("input\n"), seq, arg_access.())
+      4 -> output(seq, arg_access.())
+      5 -> jump_if_true(seq, arg_access.())
+      6 -> jump_if_false(seq, arg_access.())
+      7 -> less_than(seq, arg_access.())
+      8 -> equals(seq, arg_access.())
     end
 
     if move == :stop do
       seq
     else
-      run(at + move, seq)
+      run(move.(at), seq)
+    end
+  end
+
+  def jump_if_true(seq, [i1, move_to]) do
+    if get_in(seq, i1) != 0 do
+      {fn _pointer -> get_in(seq, move_to) end, seq}
+    else
+      {&(&1 + 3), seq}
+    end
+  end
+
+  def jump_if_false(seq, [i1, move_to]) do
+    if get_in(seq, i1) == 0 do
+      {fn _pointer -> get_in(seq, move_to) end, seq}
+    else
+      {&(&1 + 3), seq}
+    end
+  end
+
+  def less_than(seq, [i1, i2, o]) do
+    if get_in(seq, i1) < get_in(seq, i2) do
+      {&(&1 + 4), put_in(seq, o, 1)}
+    else
+      {&(&1 + 4), put_in(seq, o, 0)}
+    end
+  end
+
+  def equals(seq, [i1, i2, o]) do
+    if get_in(seq, i1) == get_in(seq, i2) do
+      {&(&1 + 4), put_in(seq, o, 1)}
+    else
+      {&(&1 + 4), put_in(seq, o, 0)}
     end
   end
 
   def sum(seq, [i1, i2, o]) do
-    put_in(seq, o, get_in(seq, i1) + get_in(seq, i2))
+    {&(&1 + 4), put_in(seq, o, get_in(seq, i1) + get_in(seq, i2))}
   end
 
   def mul(seq, [i1, i2, o]) do
-    put_in(seq, o, get_in(seq, i1) * get_in(seq, i2))
+    {&(&1 + 4), put_in(seq, o, get_in(seq, i1) * get_in(seq, i2))}
   end
 
   def put(input, seq, [a]) do
-    put_in(seq, a, String.to_integer(String.trim(input, "\n")))
+    {&(&1 + 2), put_in(seq, a, String.to_integer(String.trim(input, "\n")))}
   end
 
   def output(seq, [a]) do
     IO.puts(get_in(seq, a))
-    seq
+    {&(&1 + 2), seq}
   end
 
   def mode(n) do
@@ -73,14 +110,18 @@ defmodule Day5 do
              |> Enum.reduce("", fn x, acc -> acc <> x end)
              |> String.to_integer()
 
-    parameters = Enum.slice(list, 0..-3)
-                 |> Enum.map(&String.to_integer/1)
+    parameters = Enum.map(list, &String.to_integer/1)
 
-    parameters = if opcode == 3 || opcode == 4 do
-      parameters
-      |> Enum.slice(-3..-3)
-    else
-      parameters
+    parameters = case opcode do
+      1 -> Enum.slice(parameters, -5..-3)
+      2 -> Enum.slice(parameters, -5..-3)
+      3 -> Enum.slice(parameters, -3..-3)
+      4 -> Enum.slice(parameters, -3..-3)
+      5 -> Enum.slice(parameters, -4..-3)
+      6 -> Enum.slice(parameters, -4..-3)
+      7 -> Enum.slice(parameters, -5..-3)
+      8 -> Enum.slice(parameters, -5..-3)
+      99 -> []
     end
     {opcode, Enum.reverse(parameters)}
   end
