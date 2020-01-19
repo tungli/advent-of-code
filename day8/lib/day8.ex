cf53713eca6366f0c917e599bdefe85cbc684107
tungli <tun@mail.muni.cz>
2020-01-19 10:53:42 +0100

    complete day8

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -1,5 +1,47 @@
 defmodule Day8 do
 
+  def part_two(seq) do
+    image = parse(seq) |> shape(25, 6, nil)
+    dict = %{1 => "█", 0 => " "}
+    for row <- transpose(merge_layers(image)) do
+      Enum.map(row, fn x -> dict[x] end)
+      |> IO.puts()
+    end
+  end
+
+
+  @doc """
+  iex> Day8.transpose([[1, 2], [3, 4]])
+  [[1, 3], [2, 4]]
+  """
+  def transpose(matrix) do
+    for j <- 0..length(Enum.at(matrix, 0)) - 1 do
+      for i <- 0..length(matrix) - 1 do
+        Enum.at(Enum.at(matrix, i), j) 
+      end
+    end
+  end
+
+  @doc """
+  iex> Day8.parse("0222112222120000") |> Day8.shape(2, 2, nil)
+  ...> |> Day8.merge_layers()
+  [[0, 1], [1, 0]]
+  """
+  def merge_layers(image) do
+    for i <- 0..length(Enum.at(Enum.at(image, 0), 0)) - 1 do
+      for j <- 0..length(Enum.at(image, 0)) - 1 do
+        Enum.reduce_while(image, 0, fn x, _acc -> 
+          pixel = get_in(x, [Access.at(j), Access.at(i)])
+          if pixel < 2 do
+            {:halt, pixel}
+          else
+            {:cont, 0}
+          end
+        end)
+      end
+    end
+  end
+
   def part_one(seq) do
     image = Day8.shape(Day8.parse(seq), 25, 6, nil)
     i = find_layer_with_fewest_0(image)
